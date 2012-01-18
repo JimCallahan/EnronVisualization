dc12032ff7455ba916d65819dd5e6709a2a43850
Jim Callahan <jamesmichaelcallahan@gmail.com>
2012-01-18 18:35:57 +0200

    Initial test of SQL connection

---
+++ src/org/spiffy/db/TestConnect.scala
@@ -0,0 +1,35 @@
+package org.spiffy.db
+
+import java.sql.{ Connection, DriverManager, ResultSet }
+
+object TestConnect {
+  // Loads the JDBC driver. 
+  classOf[com.mysql.jdbc.Driver]
+
+  def main(args: Array[String]) {
+    try {
+      val connStr = "jdbc:mysql://localhost:3306/enron?user=enron&password=slimyfucks"
+      val conn = DriverManager.getConnection(connStr)
+      try {
+        // Configure to be Read Only
+        val statement = conn.createStatement(ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY)
+
+        // Execute Query
+        val rs = statement.executeQuery("SELECT body FROM bodies LIMIT 3")
+
+        // Iterate Over ResultSet
+        while (rs.next) {
+          println(rs.getString("body"))
+        }
+      } 
+      finally {
+        conn.close
+      }
+    } 
+    catch {
+      case ex =>
+        println("Uncaught Exception: " + ex.getMessage + "\n" +
+          "Stack Trace:\n" + ex.getStackTraceString)
+    }
+  }
+}
